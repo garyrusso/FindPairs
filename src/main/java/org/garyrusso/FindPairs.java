1d5353f326d0c3bf95719a55f9bd08d87c6e3103
Gary Russo <garyrusso@hotmail.com>
2014-06-26 21:21:39 -0400

    refactored

--- src/main/java/org/garyrusso/FindPairs.java
+++ src/main/java/org/garyrusso/FindPairs.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -19,19 +20,21 @@ import java.util.Map;
 
 public class FindPairs {
 
-	Map<String, List<PhraseTuple>> phraseIndex = new HashMap<String, List<PhraseTuple>>();
-	Map<String, PairTuple>         phrasePairs = new HashMap<String, PairTuple>();
-	
+	Map<String, List<PhraseTriple>> phraseIndex = new HashMap<String, List<PhraseTriple>>();
+	Map<String, PairTuple>          phrasePairs = new HashMap<String, PairTuple>();
+
 	private int phraseListNum = 0;
 
-	private class PhraseTuple
+	private class PhraseTriple
 	{
+		private String phrase;
 		private int listno;
 		private int position;
 
-		public PhraseTuple(int listno, int position)
+		public PhraseTriple(String phrase, int listno, int position)
 		{
-			this.listno = listno;
+			this.phrase   = phrase;
+			this.listno   = listno;
 			this.position = position;
 		}
 	}
@@ -48,6 +51,26 @@ public class FindPairs {
 		}
 	}
 
+	private class PairTupleComparator implements Comparator<PairTuple>
+	{
+	    @Override
+	    public int compare(PairTuple o1, PairTuple o2)
+	    {
+	    	int retVal = o2.count - o1.count;
+	    	
+	    	if (retVal == 0)
+	    	{
+	    		retVal = o1.pair.compareToIgnoreCase(o2.pair);
+	    	}
+	    	
+	    	//System.out.println("compare1: " + (o2.count - o1.count));
+	    	//System.out.println("compare2: " + o1.pair.compareToIgnoreCase(o2.pair));
+	    	//System.out.println("compare: " + o1.pair + " | " + o2.pair);
+	    	
+	        return retVal;
+	    }
+	}
+	
 	public void indexFile(File file) throws IOException
 	{
 		int pos = 0;
@@ -57,9 +80,8 @@ public class FindPairs {
 		for (String line = reader.readLine(); line != null; line = reader.readLine())
 		{
 			phraseListNum++;
-			System.out.println(phraseListNum + ". " + line);
+			//System.out.println(phraseListNum + ". " + line);
 
-			//for (String _phrase : line.split("\\W+"))
 			for (String _phrase : line.split(","))
 			{
 				String phrase = _phrase.toLowerCase();
@@ -67,19 +89,22 @@ public class FindPairs {
 
 				//System.out.println(_phrase);
 
-				List<PhraseTuple> idx = phraseIndex.get(phrase);
+				List<PhraseTriple> idx = phraseIndex.get(phrase);
 
 				if (idx == null)
 				{
-					idx = new LinkedList<PhraseTuple>();
+					idx = new LinkedList<PhraseTriple>();
 					phraseIndex.put(phrase, idx);
 				}
 
-				idx.add(new PhraseTuple(phraseListNum, pos));
+				idx.add(new PhraseTriple(_phrase, phraseListNum, pos));
 			}
 		}
 
-		System.out.println("\nindexed " + file.getPath() + " " + pos + " phrases");
+		System.out.println("");
+		System.out.println("indexed file:      " + file.getPath());
+		System.out.println("artist list count: " + phraseListNum);
+		System.out.println("artist count:      " + phraseIndex.size());
 	}
 
 	public void search(List<String> phrases)
@@ -87,7 +112,7 @@ public class FindPairs {
 		for (String _phrase : phrases)
 		{
 			String phrase = _phrase.toLowerCase();
-			List<PhraseTuple> idx = phraseIndex.get(phrase);
+			List<PhraseTriple> idx = phraseIndex.get(phrase);
 			
 			System.out.println("\nsearch phrase: " + phrase);
 			System.out.println("index size: " + idx.size());
@@ -95,22 +120,11 @@ public class FindPairs {
 
 			if (idx != null)
 			{
-				for (PhraseTuple t : idx)
+				for (PhraseTriple t : idx)
 				{
 					System.out.println("phrase: " + _phrase + " | position: " + t.position + " | listno: " + t.listno);
 				}
 			}
-		}
-	}
-
-	public void createPairsList()
-	{
-		for (Map.Entry<String, List<PhraseTuple>> entry : phraseIndex.entrySet())
-		{
-		    for (PhraseTuple t : entry.getValue())
-		    {
-			   	System.out.println(entry.getKey() + " / " + t.listno + " / " + t.position);
-		    }
 		}
 	}
 
@@ -121,34 +135,37 @@ public class FindPairs {
 	
 	public void printPairsList()
 	{
-		List<String> sortedPairs = new ArrayList<String>();
-				
+		List<PairTuple> pairTuples = new ArrayList<PairTuple>();
+		
 		for (HashMap.Entry<String, PairTuple> pairEntry : phrasePairs.entrySet())
 		{
-			sortedPairs.add(pairEntry.getKey());
-			System.out.println("111: " + pairEntry.getKey());
-			//System.out.println(pairEntry.getKey() + " / " + pairEntry.getValue().pair + " / " + pairEntry.getValue().count);
+			pairTuples.add(pairEntry.getValue());
 		}
 		
-		Arrays.sort(sortedPairs.toArray());
-		
-		for (String key : sortedPairs)
+		Collections.sort(pairTuples, new PairTupleComparator());
+
+		for (PairTuple t : pairTuples)
 		{
-			System.out.println(key + " / " + phrasePairs.get(key).pair + " / " + phrasePairs.get(key).count);
+			if (t.count > 1)
+			//if (t.count > 49)
+			{
+				System.out.println(t.pair + " / " + t.count);
+			}
 		}
 	}
-	
-	public String createHashKey(String str1, String str2) {
+
+	public String createHashKey(String str1, String str2)
+	{
 		
 		String key = "";
 		
-		String str1NoSpace = str1.replace(' ','-');
-		String str2NoSpace = str2.replace(' ','-');
+		String str1NoSpace = str1.replace(' ','-').toLowerCase();
+		String str2NoSpace = str2.replace(' ','-').toLowerCase();
 		
 		if (str1NoSpace.compareTo(str2NoSpace) > 0)
-			key = str2NoSpace + "|" + str1NoSpace;
+			key = str2NoSpace + "-" + str1NoSpace;
 		else
-			key = str1NoSpace + "|" + str2NoSpace;
+			key = str1NoSpace + "-" + str2NoSpace;
 		
 		return key;
 	}
@@ -157,28 +174,24 @@ public class FindPairs {
 	{
 		List<String> phrases = new ArrayList<String>();
 		
-		// temporary sub-index because it's the list of phrases in 1 list.
-		for (Map.Entry<String, List<PhraseTuple>> entry : phraseIndex.entrySet())
+		// temporary sub-index because it's the list of phrases in one list.
+		for (Map.Entry<String, List<PhraseTriple>> entry : phraseIndex.entrySet())
 		{
-		    for (PhraseTuple t : entry.getValue())
+		    for (PhraseTriple t : entry.getValue())
 		    {
 		    	if (t.listno == id)
 			    {
-				   	phrases.add(entry.getKey());
+				   	phrases.add(t.phrase);
 			    }
 		    }
 		}
 		
 		// Sort to so that the generated hash key is in alphanumeric order
 		Arrays.sort(phrases.toArray());
-		for (String phrase : phrases)
-		{
-			System.out.println("list " + id + " : phrase: " + phrase);
-		}
 		
 	    List<String> list = createPairsList(id, phrases);
 	    
-		System.out.println("list size: " + list.size());
+		//System.out.println("list size: " + list.size());
 	}
 
 	public void addPairsHelper(int listId, List<String> phrases)
@@ -188,18 +201,10 @@ public class FindPairs {
 		int phrasedIdx1 = 0;
 		int phrasedIdx2 = 0;
 		
-		for (int i = 0; i < phrases.size(); i++)
+		for (int i = 0; i < phrases.size()-1; i++)
 		{
-			if (i == phrases.size()-1)
-			{
-				phrasedIdx1 = 0;
-				phrasedIdx2 = i;
-			}
-			else
-			{
-				phrasedIdx1 = 0;
-				phrasedIdx2 = i + 1;
-			}
+			phrasedIdx1 = 0;
+			phrasedIdx2 = i + 1;
 			
 			pairKey = createHashKey(phrases.get(phrasedIdx1), phrases.get(phrasedIdx2));
 		
@@ -207,7 +212,7 @@ public class FindPairs {
 			
 			String pair = phrases.get(phrasedIdx1) + ", " + phrases.get(phrasedIdx2);
 	
-			System.out.println("list " + listId + " : phrase: " + pair + " : pairKey: " + pairKey);
+			//System.out.println("list " + listId + " : phrase: " + pair + " : pairKey: " + pairKey);
 
 			if (pairTuple == null)
 			{
@@ -222,10 +227,10 @@ public class FindPairs {
 	
 	public List<String> createPairsList(int id, List<String> list)
 	{
-	    for (String p : list)
-	    {
-		    System.out.println("rl: " + list.size() + " : phrase: " + p);
-	    }
+	    //for (String p : list)
+	    //{
+		//    System.out.println("rl: " + list.size() + " : phrase: " + p);
+	    //}
 		
 	    if (list.size() == 1) 
 	         return list;
@@ -244,7 +249,8 @@ public class FindPairs {
 		 * 
 		 * */
 		
-		String[] args = { "C:/projects/knewton/docs/test3.txt" };
+		//String[] args = { "D:/projects/knewton/docs/Artist_lists_small.txt" };
+		String[] args = { "D:/projects/knewton/docs/test3.txt" };
 
 		try
 		{
@@ -262,7 +268,8 @@ public class FindPairs {
 				idx.buildPairsListById(i);
 			}
 			
-			System.out.println("phrasePair size: " + idx.phrasePairs.size());
+			System.out.println("artist pair count: " + idx.phrasePairs.size());
+			System.out.println("");
 			
 			idx.printPairsList();
 		}
